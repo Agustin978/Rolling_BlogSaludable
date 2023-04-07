ae20a59b2beb7c3af1473865584ce2d22b752dd0
Agustin978 <loboagus10@gmail.com>
2023-04-07 15:37:17 -0300

    Agregadas las funcionalidades para que la pagina se le pueda cambiar el tema y el mismo sea almacenado en la memoria local de cada navegador.

---
+++ Desarrollo/js/app.js
@@ -0,0 +1,23 @@
+let temaGuardado = JSON.parse(localStorage.getItem('tema')) || 'light';
+cambiarTema(temaGuardado);
+document.
+        getElementById('btnTemaClaro').
+        addEventListener('click',()=>cambiarTema('light'));
+document.
+        getElementById('btnTemaOscuro').
+        addEventListener('click',()=>cambiarTema('dark'));
+
+function cambiarTema(tema)
+{
+    let body = document.querySelector('body');
+    let iconTema = document.getElementById('IconTema');
+    if(tema === 'dark')
+    {
+        iconTema.className = 'bi bi-moon-stars-fill';
+    }else
+    {
+        iconTema.className = 'bi bi-brightness-high-fill';
+    }
+    body.setAttribute('data-bs-theme', tema);
+    localStorage.setItem('tema',JSON.stringify(tema));
+}
